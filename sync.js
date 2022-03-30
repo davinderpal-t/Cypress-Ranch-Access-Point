f5e804e39c5c9b50a3fb6185f1ad13d6b13741a3
davinderpal-t <91445003+davinderpal-t@users.noreply.github.com>
2022-03-30 12:47:09 -0500

    Fixed rate limit
    Maximum of 2 browsers based on Heroku RAM

--- sync.js
+++ sync.js
@@ -14,7 +14,7 @@ class Farm {
             this.workers = workerFarm(require.resolve('./index'));
         }
         return new Promise((res, rej) => {
-            if (this.open - this.finished > 2) {
+            if (this.open - this.finished > 1) {
                 setTimeout(() => {
                     res(this.run(user, pass));
                 }, 1000);
